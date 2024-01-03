67452321efba239cad8cb46950af5490e67d7e60
DABHI AASHISH BHARATBHAI <150463240+aashish-b-dabhi@users.noreply.github.com>
2024-01-03 22:58:07 +0530

    Add files via upload

---
+++ fileheadling.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+main()
+{
+	int *p;
+	char data[1000];
+	p = fopen("fileheadling.txt","r");
+	
+	if(p==0)
+	{
+		printf("file not open");
+		
+	}
+	else
+	{
+		while(fgets(data,5,p)!=NULL)
+		{
+			printf("%s",data);
+			
+		}
+	}
+	fclose(p);
+}
